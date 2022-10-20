d2dcb02518cf6e9f8a4ea590ca05806d8ede4f5a
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2022-10-20 19:53:28 +0200

    accel/igna: initialize MMU
    Setup MMU in the driver with a new memory component.
    
    Signed-off-by: Tomasz Jankowski <tomasz1.jankowski@intel.com>
    Tested-by: Mikolaj Grzybowski <mikolajx.grzybowski@intel.com>
    Co-developed-by: Jianxun Zhang <jianxun.zhang@linux.intel.com>
    Signed-off-by: Jianxun Zhang <jianxun.zhang@linux.intel.com>
    Co-developed-by: Maciej Kwapulinski <maciej.kwapulinski@linux.intel.com>
    Signed-off-by: Maciej Kwapulinski <maciej.kwapulinski@linux.intel.com>
    Signed-off-by: Alexander F. Lent <lx@xanderlent.com>

---
+++ drivers/accel/igna/gna_mem.h
@@ -0,0 +1,50 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/* Copyright(c) 2017-2022 Intel Corporation */
+
+#ifndef __GNA_MEM_H__
+#define __GNA_MEM_H__
+
+#include <linux/types.h>
+
+#include "gna_hw.h"
+
+struct gna_gem_object;
+struct work_struct;
+struct gna_device;
+
+struct gna_xnn_descriptor {
+	u32 labase;
+	u16 lacount;
+	u16 _rsvd;
+};
+
+struct gna_mmu {
+	u32 vamaxaddr;
+	u8 __res_204[12];
+	u32 pagedir_n[GNA_PGDIRN_LEN];
+};
+
+struct gna_hw_descriptor {
+	u8 __res_0000[256];
+	struct gna_xnn_descriptor xnn_config;
+	u8 __unused[248];
+	struct gna_mmu mmu;
+};
+
+struct gna_mmu_object {
+	struct gna_hw_descriptor *hwdesc;
+
+	dma_addr_t hwdesc_dma;
+
+	u32 **pagetables;
+	dma_addr_t *pagetables_dma;
+
+	u32 num_pagetables;
+
+	u32 filled_pts;
+	u32 filled_pages;
+};
+
+int gna_mmu_init(struct gna_device *gna_priv);
+
+#endif // __GNA_MEM_H__
