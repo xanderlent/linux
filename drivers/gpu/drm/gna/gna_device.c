559cd76971188b5ad56a5360915bbe72132ead55
Maciej Kwapulinski <maciej.kwapulinski@linux.intel.com>
2022-10-20 19:53:25 +0200

    gna: add PCI driver module
    Add a new PCI driver for Intel(R) Gaussian & Neural Accelerator
    with basic support like module loading and unloading. The full
    function of the driver will be added by further changes.
    
    Alexander F. Lent: Adapt to upstream changes between v6.0 and v6.9
    
    Signed-off-by: Maciej Kwapulinski <maciej.kwapulinski@linux.intel.com>
    Tested-by: Mikolaj Grzybowski <mikolajx.grzybowski@intel.com>
    Co-developed-by: Tomasz Jankowski <tomasz1.jankowski@intel.com>
    Signed-off-by: Tomasz Jankowski <tomasz1.jankowski@intel.com>
    Co-developed-by: Jianxun Zhang <jianxun.zhang@linux.intel.com>
    Signed-off-by: Jianxun Zhang <jianxun.zhang@linux.intel.com>
    Signed-off-by: Alexander F. Lent <lx@xanderlent.com>

---
+++ drivers/gpu/drm/gna/gna_device.c
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-only
+// Copyright(c) 2017-2022 Intel Corporation
+
+#include <linux/module.h>
+
+MODULE_AUTHOR("Intel Corporation");
+MODULE_DESCRIPTION("Intel(R) Gaussian & Neural Accelerator (Intel(R) GNA) Driver");
+MODULE_LICENSE("GPL");
