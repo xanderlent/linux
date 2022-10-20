c7ca40b278b812546a9addc721abe5f8a570fe93
Maciej Kwapulinski <maciej.kwapulinski@linux.intel.com>
2022-10-20 19:53:26 +0200

    gna: add GNA DRM device
    Signed-off-by: Maciej Kwapulinski <maciej.kwapulinski@linux.intel.com>
    Tested-by: Mikolaj Grzybowski <mikolajx.grzybowski@intel.com>

--- drivers/gpu/drm/gna/gna_device.h
+++ drivers/gpu/drm/gna/gna_device.h
@@ -4,6 +4,27 @@
 #ifndef __GNA_DEVICE_H__
 #define __GNA_DEVICE_H__
 
-#define DRIVER_NAME		"gna"
+#include <drm/drm_device.h>
 
+#include "gna_hw.h"
+
+#define DRIVER_NAME		"gna"
+#define DRIVER_DESC		"Intel(R) Gaussian & Neural Accelerator (Intel(R) GNA)"
+#define DRIVER_DATE		"20211201"
+
+#define DRIVER_MAJOR		1
+#define DRIVER_MINOR		0
+#define DRIVER_PATCHLEVEL	0
+
+struct device;
+
+struct gna_device {
+	struct drm_device drm;
+
+	/* device related resources */
+	void __iomem *iobase;
+	struct gna_dev_info info;
+};
+
+int gna_probe(struct device *parent, struct gna_dev_info *dev_info, void __iomem *iobase);
 #endif /* __GNA_DEVICE_H__ */
