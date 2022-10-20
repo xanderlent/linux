4d191f4f31f8db1b568156c466be925a446436e3
Tomasz Jankowski <tomasz1.jankowski@intel.com>
2022-10-20 19:53:31 +0200

    gna: add GNA_COMPUTE ioctl
    This is the ioctl context part of score request + "do nothing" work queue
    implementation. Work queue context actual part will be added by next patch.
    
    Alexander F. Lent: Move line from previous commit to this one, fix build
    
    signed-off-by: Tomasz Jankowski <tomasz1.jankowski@intel.com>
    Tested-by: Mikolaj Grzybowski <mikolajx.grzybowski@intel.com>
    Co-developed-by: Anisha Dattatraya Kulkarni <anisha.dattatraya.kulkarni@intel.com>
    Signed-off-by: Anisha Dattatraya Kulkarni <anisha.dattatraya.kulkarni@intel.com>
    Co-developed-by: Jianxun Zhang <jianxun.zhang@linux.intel.com>
    Signed-off-by: Jianxun Zhang <jianxun.zhang@linux.intel.com>
    Co-developed-by: Maciej Kwapulinski <maciej.kwapulinski@linux.intel.com>
    Signed-off-by: Maciej Kwapulinski <maciej.kwapulinski@linux.intel.com>
    Signed-off-by: Alexander F. Lent <lx@xanderlent.com>

--- drivers/gpu/drm/gna/gna_device.h
+++ drivers/gpu/drm/gna/gna_device.h
@@ -7,7 +7,10 @@
 #include <drm/drm_device.h>
 #include <drm/drm_gem_shmem_helper.h>
 
+#include <linux/atomic.h>
 #include <linux/io.h>
+#include <linux/list.h>
+#include <linux/mutex.h>
 #include <linux/types.h>
 
 #include "gna_gem.h"
@@ -22,6 +25,7 @@
 #define DRIVER_MINOR		0
 #define DRIVER_PATCHLEVEL	0
 
+struct workqueue_struct;
 union gna_parameter;
 struct drm_file;
 struct device;
@@ -37,6 +41,15 @@ struct gna_device {
 	struct gna_hw_info hw_info;
 
 	struct gna_mmu_object mmu;
+
+	struct list_head request_list;
+	/* protects request_list */
+	struct mutex reqlist_lock;
+	struct workqueue_struct *request_wq;
+	atomic_t request_count;
+
+	/* requests that are in queue to be run +1 for currently processed one */
+	atomic_t enqueued_requests;
 };
 
 int gna_probe(struct device *parent, struct gna_dev_info *dev_info, void __iomem *iobase);
@@ -51,9 +64,17 @@ int gna_gem_new_ioctl(struct drm_device *dev, void *data,
 int gna_gem_free_ioctl(struct drm_device *dev, void *data,
 		struct drm_file *file);
 
+int gna_score_ioctl(struct drm_device *dev, void *data,
+		struct drm_file *file);
+
 static inline u32 gna_reg_read(struct gna_device *gna_priv, u32 reg)
 {
 	return readl(gna_priv->iobase + reg);
+}
+
+static inline const char *gna_name(struct gna_device *gna_priv)
+{
+	return gna_priv->drm.unique;
 }
 
 static inline struct device *gna_dev(struct gna_device *gna_priv)
